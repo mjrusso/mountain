8b807c5e21a4ade950f004e6bdd16701a923c565
Michael Russo <mjrusso@gmail.com>
2014-08-10 10:37:30 -0400

    First cut of join feature.

---
+++ mountain/join.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+import re
+
+
+# The extensions that we will automatically try in the event that the user
+# does not supply an extension when referencing a file in an `#include`
+# directive.
+KNOWN_EXTENSIONS = (
+    ".fountain",
+    ".txt",
+    ".md",
+    ".mdown",
+    ".markdown",
+)
+
+
+def expand_manifest(manifest_path):
+
+    with codecs.open(manifest_path, "r", "utf-8") as f:
+        manifest = f.read()
+
+    base_path = os.path.dirname(os.path.abspath(manifest_path))
+
+    referenced_file_names = (re.compile("\[\[\s*#include\s+(.*)\s*\]\]")
+                               .findall(manifest))
+
+    for file_name in referenced_file_names:
+
+        for extension in ("",) + KNOWN_EXTENSIONS:
+
+            file_path = os.path.join(base_path, file_name + extension)
+
+            if os.path.exists(file_path):
+
+                with codecs.open(file_path, "r", "utf-8") as f:
+                    referenced_file = f.read()
+
+                manifest = re.sub(
+                    "\[\[\s*#include\s+%s\s*\]\]" % file_name,
+                    "\n\n".join([
+                        "[[#reference %s]]" % file_name,
+                         referenced_file
+                    ]),
+                    manifest)
+
+                break
+
+    return manifest
--- mountain/mountain.py
+++ mountain/mountain.py
@@ -2,16 +2,45 @@
 
 __version__ = "0.1.0"
 
+import codecs
 import sys
+
+from .join import expand_manifest
 
 
 def foo(a, b):
     return a + b + 2
 
 
+def join(manifest_path, output_path):
+    print("Reading manifest from `%s`." % manifest_path)
+    with codecs.open(output_path, "w", "utf-8") as f:
+        manifest = expand_manifest(manifest_path)
+        f.write(manifest)
+    print("Wrote expanded manifest to `%s`." % output_path)
+
+def split(manifest_path, output_path):
+    pass
+
+def help():
+    version()
+
+def version():
+    print("Mountain v%s." % __version__)
+
 def main():
     args = sys.argv[1:]
-    print("Executing mountain version %s." % __version__)
-    print("List of argument strings: %s" % args)
-    print("Hi, this is Mountain!")
-    print("Foo %s" % foo(1,3))
+
+    if not len(args):
+        help()
+    elif args[0] == "join":
+        join(args[1], args[2])
+    elif args[0] == "split":
+        split(args[1], args[2])
+    elif args[0] == "--version":
+        version()
+    elif args[0] == "--help":
+        help()
+    else:
+        print("Invalid option specified.")
+        help()
