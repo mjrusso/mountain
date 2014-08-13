e5e811da2b5913fd79ed65b89368dd1739ccffe8
Michael Russo <mjrusso@gmail.com>
2014-08-12 21:49:57 -0400

    Require file extensions to be specified in directives.
    `mountain join` used to automatically try a list of known file
    extensions, in the event that the user did not provide a file extension
    when referencing a file in an #include directive.
    
    Thus, you could write something like this:
    
        [[#include intro]]
    
    And that file would automatically be found, provided that a file named
    `intro` existed with a `.fountain`, `.txt`, `.md`, `.mdown`, or
    `.markdown`extension.
    
    This commit drops support for this magic. Instead, you'll need to write:
    
        [[#include intro.fountain]]
    
    (Or use whatever file extension you prefer.)
    
    Although the code was there, and it worked, it required some magic that
    may be confusing to explain to users and I'm generally worried that it
    will introduce more issues than it solves.
    
    It wasn't yet implemented in the reverse case (`mountain split`), but
    implementation is feasible. Here's the algorithm:
    
    - check if file_path ends with an extension
        - (if yes, write the file; return)
    - check if file_path exists
        - (if yes, write the file; return)
    - otherwise, file_path does not exist
        - for each supported extension, add the extension to file_path and check if
          the file_path exists
          - if it does, write the file; break
          - else, create a new file with the default extension (.fountain)

--- mountain/join.py
+++ mountain/join.py
@@ -5,18 +5,6 @@ import os
 import re
 
 
-# The extensions that we will automatically try in the event that the user
-# does not supply an extension when referencing a file in an `#include`
-# directive.
-KNOWN_EXTENSIONS = (
-    ".fountain",
-    ".txt",
-    ".md",
-    ".mdown",
-    ".markdown",
-)
-
-
 def expand_manifest(manifest_path):
 
     with codecs.open(manifest_path, "r", "utf-8") as f:
@@ -30,27 +18,22 @@ def expand_manifest(manifest_path):
     for file_name in referenced_file_names:
 
         file_name = file_name.strip()
+        file_path = os.path.join(base_path, file_name)
 
-        for extension in ("",) + KNOWN_EXTENSIONS:
+        if os.path.exists(file_path):
 
-            file_path = os.path.join(base_path, file_name + extension)
+            with codecs.open(file_path, "r", "utf-8") as f:
+                referenced_file = f.read()
 
-            if os.path.exists(file_path):
-
-                with codecs.open(file_path, "r", "utf-8") as f:
-                    referenced_file = f.read()
-
-                manifest = re.sub(
-                    "\[\[\s*#include\s+%s\s*\]\]" % file_name,
-                    "".join([
-                        "[[#reference %s]]" % file_name,
-                        "\n" if referenced_file.startswith("\n") else "\n\n",
-                        referenced_file,
-                        "\n" if referenced_file.endswith("\n") else "\n\n",
-                        "[[#reference-end]]"
-                    ]),
-                    manifest)
-
-                break
+            manifest = re.sub(
+                "\[\[\s*#include\s+%s\s*\]\]" % file_name,
+                "".join([
+                    "[[#reference %s]]" % file_name,
+                    "\n" if referenced_file.startswith("\n") else "\n\n",
+                    referenced_file,
+                    "\n" if referenced_file.endswith("\n") else "\n\n",
+                    "[[#reference-end]]"
+                ]),
+                manifest)
 
     return manifest
