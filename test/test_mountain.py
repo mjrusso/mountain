4cc68bd6b95c3177b50c08890bbae5b2b14ef311
Michael Russo <mjrusso@gmail.com>
2014-08-13 09:48:53 -0400

    Add split test.

--- test/test_mountain.py
+++ test/test_mountain.py
@@ -4,6 +4,7 @@ import codecs
 import os
 
 from mountain.join import expand_manifest
+from mountain.split import split_combined_document
 
 
 class TestExpandManifest(object):
@@ -53,5 +54,39 @@ class TestExpandManifest(object):
         ).read()
 
         expected = [{"path": "test-path", "contents": expected_contents}]
+
+        assert actual == expected
+
+
+class TestSplitCombinedDocument(object):
+
+    def test_split_combined_document(self):
+
+        actual = split_combined_document(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "fixtures",
+                "example",
+                "manifest.fountain"),
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "fixtures",
+                "example",
+                "screenplay.fountain"))
+
+        expected = list()
+
+        for path in ["fixtures/example/intro.fountain",
+                     "fixtures/example/inciting-incident.fountain",
+                     "fixtures/example/the-end.fountain",
+                     "fixtures/example/manifest.fountain"]:
+
+            path = os.path.join(
+                       os.path.dirname(os.path.realpath(__file__)),
+                       path)
+
+            expected.append(
+                {"path": path,
+                 "contents": codecs.open(path, "r", "utf-8").read()})
 
         assert actual == expected
