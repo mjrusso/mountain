d3273e2bced05a290e407e68e17b2506350664d1
Michael Russo <mjrusso@gmail.com>
2014-08-12 22:00:18 -0400

    Hook up example fixture to test.

--- test/test_mountain.py
+++ test/test_mountain.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import codecs
 import os
 
 from mountain.join import expand_manifest
@@ -7,7 +8,28 @@ from mountain.join import expand_manifest
 
 class TestExpandManifest(object):
 
-    def test_expand_manifest(self):
+    def test_expand_manifest_simple(self):
+
+        actual = expand_manifest(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "fixtures",
+                "example",
+                "manifest.fountain"))
+
+        expected = codecs.open(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "fixtures",
+                "example",
+                "screenplay.fountain"),
+            "r",
+            "utf-8"
+        ).read()
+
+        assert actual == expected
+
+    def test_expand_manifest_complex(self):
 
         actual = expand_manifest(
             os.path.join(
@@ -16,11 +38,14 @@ class TestExpandManifest(object):
                 "join",
                 "manifest.fountain"))
 
-        expected = open(
+        expected = codecs.open(
             os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
                 "fixtures",
                 "join",
-                "screenplay.fountain")).read()
+                "screenplay.fountain"),
+            "r",
+            "utf-8"
+        ).read()
 
         assert actual == expected
